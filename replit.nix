a2c3f288504ebf292bf931ab6d607f0e31d85925
ff6347 <hash@ff6347.email>
2024-09-10 13:59:28 +0200

    chore: Remove replit config files

--- replit.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs }: {
-  deps = [
-		pkgs.nodePackages.prettier
-    pkgs.vim
-    pkgs.nodejs
-    pkgs.nodePackages.vscode-langservers-extracted
-    pkgs.nodePackages.typescript-language-server
-  ];
-}
